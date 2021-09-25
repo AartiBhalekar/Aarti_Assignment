6de1692f7ddebb70b62e2f888857f63e866d0f96
AartiBhalekar <90775695+AartiBhalekar@users.noreply.github.com>
2021-09-25 15:17:07 +0530

    Delete Aarti'sAssignments/Day8 directory

--- Aarti'sAssignments/Day8/Que3/Spring-Boot-Jpa-mysql2-1-1-5/src/main/java/com/cj/spring/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cj.spring.repository;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-import com.cj.spring.bean.*;
-public interface AddressRepository extends CrudRepository<Address, Long>
-{
-	
-}
--- Aarti'sAssignments/Day8/Que3/Spring-Boot-Jpa-mysql2-1-1-5/src/main/java/com/cj/spring/repository/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cj.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.cj.spring.bean.Location;
-
-public interface LocationRepository extends CrudRepository<Location, Long> {
-
-}
